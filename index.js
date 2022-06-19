48c8ec5b3f66a055b49aeb90db3359a4c6523411
Vivek Patel <vpatel360@gmail.com>
2022-06-19 02:12:42 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,84 @@
 // Follow along with the examples here
+
+/*
+function doNothing() {}
+
+function sayHello() {
+    console.log("Hello!");
+}
+sayHello();
+
+function sayHelloToIsabel () {
+    console.log("Hello, Isabel!");
+}
+sayHelloToIsabel ();
+
+function sayHelloToSofia () {
+    console.log("Hello, Sofia!");
+}
+sayHelloToSofia ();
+
+function sayHelloToBrendan () {
+    console.log("Hello, Brendan!");
+}
+sayHelloToBrendan ();
+
+sayHelloToIsabel ();
+sayHelloToSofia ();
+sayHelloToBrendan ();
+
+function doSomething(thing) {
+    console.log(thing);
+}
+doSomething("anything");
+
+
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+}
+
+sayHelloTo("Isabel");
+sayHelloTo("Jane");
+sayHelloTo("R2-D2");
+sayHelloTo(1);
+
+function say(greeting, firstName) {
+    console.log("firstName: ", firstName);
+    console.log("greeting: ", greeting);
+    console.log(`${greeting}, ${firstName}!`);
+  }  
+
+say("Julio", "Hello");
+
+function add(x, y){
+    return x + y;
+}
+console.log(add(1, 2));
+
+function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+
+  console.log(say("Hello", "Sofia"));
+
+
+  function add(x, y) {
+    return x + y;
+  }
+
+  const num1 = 1;
+  const num2 = 5;
+
+  const message = `The sum of your numbers is ${add(num1, num2)}`;
+
+  console.log(message);
+
+
+
+function say(greeting, firstName) {
+    console.log("I was called!");
+    return `${greeting}, ${firstName}!`;
+  }
+console.log(say("Howdy", "partner"));
+
+*/
